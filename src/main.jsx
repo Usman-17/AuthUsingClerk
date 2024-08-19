a2be507cdf7aaf95079e9cb03aa9b631b50aad7c
Usman-17 <usmandev580@gmail.com>
2024-08-19 15:39:57 +0500

    Install Clerk SDK for user authentication

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ClerkProvider, ClerkLoaded, ClerkLoading } from "@clerk/clerk-react";
+import Loader from "./components/Loader.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
+createRoot(document.getElementById("root")).render(
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <ClerkLoading>
+      <Loader />
+    </ClerkLoading>
+
+    <ClerkLoaded>
+      <App />
+    </ClerkLoaded>
+  </ClerkProvider>
+);
